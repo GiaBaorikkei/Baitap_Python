24a412b7bc09e3c1c180757ffc2c43ad01f9339b
Gia Bảo <m1procare@MacBook-Pro-cua-Gia-Bao.local>
2025-11-18 00:09:18 +0700

    session01

---
+++ Session01/Bai4.py
@@ -0,0 +1,2 @@
+number = input("Nhập chuỗi số thập phân: ")
+print(f"Số bạn sau khi chuyển đổi là : {float(number)}")
---
+++ Session01/Bai8.py
@@ -0,0 +1,2 @@
+number = float(input("Nhập số của bạn : "))
+print(f"Số bạn sau khi chuyển đổi là : {int(number)}")
